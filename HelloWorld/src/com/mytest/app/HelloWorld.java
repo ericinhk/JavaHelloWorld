36ac356d27a80e6128c171da141dfc12a1ad1002
Eric <Eric@ERIC-PC>
2019-03-10 15:33:37 +0800

    commit 3

--- HelloWorld/src/com/mytest/app/HelloWorld.java
+++ HelloWorld/src/com/mytest/app/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World! My name is Eric!");
 		
-		System.out.println("In this branch, I would like to do a for-loop");
+		System.out.println("In this branch, I would like to do a for-loop for 10");
 		for (int i = 0; i < 10; i++) {
-			System.out.println(""+i);
+			System.out.println("Item: "+(i+1));
 		}
 	}
 }
