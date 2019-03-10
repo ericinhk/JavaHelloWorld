6f9f652e32e64c29510d62cf2d37c87fb6d99eeb
Eric <Eric@ERIC-PC>
2019-03-10 15:22:46 +0800

    Trying to commit changes to new branch.

--- HelloWorld/src/com/mytest/app/HelloWorld.java
+++ HelloWorld/src/com/mytest/app/HelloWorld.java
@@ -3,5 +3,10 @@ package com.mytest.app;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World! My name is Eric!");
+		
+		System.out.println("In this branch, I would like to do a for-loop");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(""+i);
+		}
 	}
 }
