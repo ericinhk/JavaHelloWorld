0a13172a5cdc0683354da134f9e074e0fa9b2a09
Eric <Eric@ERIC-PC>
2019-03-10 15:13:24 +0800

    This is my 2nd commit, just make a little bit changes.

--- HelloWorld/src/com/mytest/app/HelloWorld.java
+++ HelloWorld/src/com/mytest/app/HelloWorld.java
@@ -2,6 +2,6 @@ package com.mytest.app;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! My name is Eric!");
 	}
 }
